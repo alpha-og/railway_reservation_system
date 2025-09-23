cbb6eae4d69ad15664d435c414e4868ede5cc38d
JVP555 <jilvarghese.2005@gmail.com>
2025-09-24 02:29:31 +0530

    fix: path to trainlist

--- client/src/routes/admin/index.jsx
+++ client/src/routes/admin/index.jsx
@@ -1,16 +1,5 @@
-/*import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/admin/')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/admin/index"!</div>
-}*/
-
-
 import { createFileRoute } from '@tanstack/react-router';
-import TrainList from '../../../features/admin/trains/components/TrainList';
+import TrainList from '../../features/admin/trains/components/TrainList';
 
 export const Route = createFileRoute('/admin/')({
   component: TrainList,
