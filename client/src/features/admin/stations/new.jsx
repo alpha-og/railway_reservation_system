0ec8d3c0d82cf9a323877254deee7a264c7a890c
JVP555 <jilvarghese.2005@gmail.com>
2025-09-24 02:23:40 +0530

    fix(stations): removed reduntant components and routes

--- client/src/features/admin/stations/new.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import StationForm from './components/StationForm';
-
-const NewStationPage = () => {
-  return <StationForm isEditing={false} />;
-};
-
-export default NewStationPage;
