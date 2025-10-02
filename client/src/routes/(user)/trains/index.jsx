4e9903b774cc473554bec98a4cf1ce63733e9cb3
Athul Anoop <athulanoop2020@gmail.com>
2025-10-02 13:16:49 +0530

    feat: layout updated

--- client/src/routes/(user)/trains/index.jsx
+++ client/src/routes/(user)/trains/index.jsx
@@ -11,7 +11,7 @@ function RouteComponent() {
   return (
     <section
       id="search"
-      className="h-full p-6 flex flex-col items-center justify-center gap-4 bg-base-200 md:flex-row"
+      className="w-full h-max p-6 flex flex-col items-center justify-center gap-4 bg-base-200 md:h-full md:flex-row md:items-start md:justify-between"
     >
       <TrainForm />
       <TrainGallery />
