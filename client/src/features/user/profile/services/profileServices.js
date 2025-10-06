1d23fb788449caae850b73669e99ebeccb1574f2
Athul Anoop <athulanoop2020@gmail.com>
2025-10-07 00:22:28 +0530

    fix: profile page

--- client/src/features/user/profile/services/profileServices.js
+++ client/src/features/user/profile/services/profileServices.js
@@ -1,48 +1,23 @@
 // src/features/user/profile/services/profileServices.js
 import axiosClient from "../../../../services/config/axiosClient.js";
 import { useAuthStore } from "../../../../store/useAuthStore.js";
-import { useApiWithFallback } from "../../../../services/useApiWithFallback.js";
-import { useUserId } from "../../../../hooks/useUserId.js";
 
 // Helper: attach token header
 const createAuthHeaders = (token) =>
   token ? { Authorization: `Bearer ${token}` } : {};
 
-// --- Hook: Get logged-in user profile ---
-export const useGetProfile = () => {
-  const token = useAuthStore((state) => state.token);
-  const userId = useUserId();
-
-  return useApiWithFallback({
-    fallbackKey: `profileByUser_${userId || "no_user"}`,
-    endpoint: async () => {
-      if (!userId) {
-        throw new Error("No user ID available");
-      }
-
-      const res = await axiosClient.get(`/users/${userId}`, {
-        headers: createAuthHeaders(token),
-      });
-
-      return res.data;
-    },
-  });
-};
-
 // --- Hook: Update logged-in user profile ---
 export const useUpdateProfile = () => {
   const token = useAuthStore((state) => state.token);
-  const userId = useUserId();
 
   return async (data) => {
-    if (!userId) {
-      throw new Error("No user ID available for profile update");
-    }
-
-    const res = await axiosClient.put(`/users/${userId}`, data, {
+    // Remove password from data since backend doesn't support it
+    const { password, ...profileData } = data;
+    
+    const res = await axiosClient.patch(`/profile/update`, profileData, {
       headers: createAuthHeaders(token),
     });
 
-    return res.data;
+    return res.data.data.profile;
   };
 };
